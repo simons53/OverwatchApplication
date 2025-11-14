c35040102ad33cf37918c6229cab6c0cf1700125
hvittorelli <misshopev@gmail.com>
2025-11-13 20:42:51 -0500

    Added Classes and Seed Data

---
+++ OverwatchApplication/Models/HeroNote.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace OverwatchApplication.Models
+{
+    public class HeroNote
+    {
+        public int NoteID { get; set; } // PK
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+        public float HoursPlayed { get; set; }
+
+        // FKs
+        public int HeroID { get; set; }
+        public Hero Hero { get; set; }
+
+        public int UserID { get; set; }
+        public User User { get; set; }
+
+        // Methods
+        public void UpdateNoteContent(string newContent)
+        {
+            // Implement in Sprint 3
+        }
+
+        public void UpdateHours(float newHours)
+        {
+            // Implement in Sprint 3
+        }
+    }
+}
---
+++ OverwatchApplication/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace OverwatchApplication.Models 
+{ 
+    public class User 
+    { 
+        public int UserID { get; set; } //PK
+        public string UserName { get; set; } 
+        public string Email { get; set; } 
+        public string PasswordHash { get; set; } 
+        
+        // Navigation Properties
+        public List<HeroNote> Notes { get; set; } = new(); 
+        
+        // Methods
+        public void AddNote(HeroNote note) 
+        { 
+            // Implement in Sprint 3
+        } 
+        public void DeleteNote(int noteID) 
+        { 
+            // Implement in Sprint 3
+        } 
+        public List<HeroNote> GetHeroNotes(int heroID) 
+        { 
+            // Implement in Sprint 3
+            return new List<HeroNote>(); 
+        } 
+    } 
+}
